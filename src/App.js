db8c211da7aff800899b3b1faa663176bd70f20a
Ashwini <ashwiniparaye9309@gmail.com>
2023-02-27 15:09:44 +0530

    created routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Modal from './Modal';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 function App() {
+
   return (
-    <div>App</div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/modal" element={<Modal />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
